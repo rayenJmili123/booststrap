027904bb7f31c2a94cd054ccc5b41852f719190e
rayenJmili123 <53777010+rayenJmili123@users.noreply.github.com>
2019-08-16 14:25:16 +0200

    Add files via upload

---
+++ GoMyCode/Rayen GMC Niv1/editor.js
@@ -0,0 +1,95 @@
+$(".course1").hover(function(){
+    $(".btnCourses1").toggleClass('btnCourses1Hover')
+})
+
+$(".course2").hover(function(){
+    $(".btnCourses2").toggleClass('btnCourses1Hover')
+})
+
+$(".course3").hover(function(){
+    $(".btnCourses3").toggleClass('btnCourses1Hover')
+})
+
+$(".course4").hover(function(){
+    $(".btnCourses4").toggleClass('btnCourses1Hover')
+})
+
+$(".course5").hover(function(){
+    $(".btnCourses5").toggleClass('btnCourses1Hover')
+})
+
+$(".course6").hover(function(){
+    $(".btnCourses6").toggleClass('btnCourses1Hover')
+})
+
+$(".course7").hover(function(){
+    $(".btnCourses7").toggleClass('btnCourses1Hover')
+})
+
+$(".course8").hover(function(){
+    $(".btnCourses8").toggleClass('btnCourses1Hover')
+})
+
+$(".course9").hover(function(){
+    $(".btnCourses9").toggleClass('btnCourses1Hover')
+})
+
+
+$(".open").on("click", function(){
+    $(".popup, .popup-content").addClass("active");
+    });
+
+    $(".close, .popup").on("click", function(){
+        $(".popup, .popup-content").removeClass("active");
+        });
+
+       $(".course1").click(function(){
+        $("#ex1").modal({
+            fadeDuration: 100
+          });
+        })
+        
+        $(".course2").click(function(){
+            $("#ex1").modal({
+                fadeDuration: 100
+              });
+            })
+
+            $(".course3").click(function(){
+                $("#ex1").modal({
+                    fadeDuration: 100
+                  });
+                })
+                $(".course4").click(function(){
+                    $("#ex1").modal({
+                        fadeDuration: 100
+                      });
+                    })
+                    $(".course5").click(function(){
+                        $("#ex1").modal({
+                            fadeDuration: 100
+                          });
+                        })
+                        $(".course6").click(function(){
+                            $("#ex1").modal({
+                                fadeDuration: 100
+                              });
+                            })
+                            $(".course7").click(function(){
+                                $("#ex1").modal({
+                                    fadeDuration: 100
+                                  });
+                                })
+                                $(".course8").click(function(){
+                                    $("#ex1").modal({
+                                        fadeDuration: 100
+                                      });
+                                    })
+                                    $(".course9").click(function(){
+                                        $("#ex1").modal({
+                                            fadeDuration: 100
+                                          });
+                                        }) 
+                                        $(".modal").load(function(){
+                                            $("#ex1").hide();
+                                          });
